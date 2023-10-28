0353f90611cbb2754dfdd5600880f4d41cd19252
Turek-Piotr <turekpiotr778@gmail.com>
2023-10-28 22:20:59 +0200

    Add new files

---
+++ projekte1.js
@@ -0,0 +1,27 @@
+function calculate(){
+    const num1 = parseInt(document.getElementById("num1").value);
+    const char = document.getElementById("char").value;
+    const num2 = parseInt(document.getElementById("num2").value);
+
+    let result
+
+    if (isNaN(num1) || isNaN(num2)) {
+        result = "Proszę wprowadzić poprawne liczby w obu polach.";
+    } else {
+        if (char == "+") {
+            result = num1 + num2;
+        } else if (char == "-") {
+            result = num1 - num2;
+        } else if (char == "*") {
+            result = num1 * num2;
+        } else if (char == "/") {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                result = "Nie można dzielić przez 0!";
+            }
+        }
+
+        document.getElementById("res").textContent = "Wynik to: " +result;
+    }
+}
