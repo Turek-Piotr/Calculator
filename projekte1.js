a79ef96ad47685469166a67c7879bb936e0dc866
Piotr Turek <149108980+Turek-Piotr@users.noreply.github.com>
2024-04-21 21:13:50 +0200

    projekte1.js

--- projekte1.js
+++ projekte1.js
@@ -3,21 +3,27 @@ function calculate(){
     const char = document.getElementById("char").value;
     const num2 = parseInt(document.getElementById("num2").value);
 
-    let result
+    let result;
 
-        if (char == "+") {
-            result = num1 + num2;
-        } else if (char == "-") {
-            result = num1 - num2;
-        } else if (char == "*") {
-            result = num1 * num2;
-        } else if (char == "/") {
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                result = "Nie można dzielić przez 0!";
-            }
+    if (char == "+") {
+        result = num1 + num2;
+    } else if (char == "-") {
+        result = num1 - num2;
+    } else if (char == "*") {
+        result = num1 * num2;
+    } else if (char == "/") {
+        if (num2 != 0) {
+            result = num1 / num2;
+        } else {
+            result = "Dzielenie przez zero jest niedozwolone";
         }
+    } else if (char == "^") {
+        result = Math.pow(num1, num2);  
+    }
+    
+    document.getElementById("res").textContent = "Wynik to: " + result;
 
-        document.getElementById("res").textContent = "Wynik to: " +result;
+    const historyDiv = document.getElementById("history");
+    const operation = num1 + " " + char + " " + num2 + " = " + result;
+    historyDiv.innerHTML += operation + "<br>";
 }
